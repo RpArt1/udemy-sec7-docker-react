33c906c26a75525f0ab600367fe36d63c8c0cc72
artur.rapczynski <artur.rapczynski@hycom.pl>
2021-01-13 13:20:47 +0100

    added travix file

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('mdlkasdmlaks mldkaml() => {
+test('test () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
